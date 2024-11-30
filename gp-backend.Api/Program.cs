7fafe75b6c5c6624cc6eb82a85dc212c4131e288
Mohamed Alawady <101016792+moal223@users.noreply.github.com>
2024-11-30 21:47:04 +0200

    edit cors

--- gp-backend.Api/Program.cs
+++ gp-backend.Api/Program.cs
@@ -77,7 +77,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", builder =>
     {
-        builder.WithOrigins("http://localhost:5173")
+        builder.AllowAnyOrigin()
         .AllowAnyMethod()
         .AllowAnyHeader()
         .AllowCredentials();  
