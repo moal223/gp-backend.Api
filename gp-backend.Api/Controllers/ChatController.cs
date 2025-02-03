7faf8a7276badc922466c3d2b5d3e4411fe010b4
Mohamed Alawady <101016792+moal223@users.noreply.github.com>
2025-02-03 20:52:50 +0200

    change constring

--- gp-backend.Api/Controllers/ChatController.cs
+++ gp-backend.Api/Controllers/ChatController.cs
@@ -66,7 +66,7 @@ namespace gp_backend.Api.Controllers
                 RecipientId = receiverId,
                 FilePath = $"SkinScan/{fileName}",
                 FileUrl = fileUrl,
-                FileName = file.FileName,
+                FileName = fileName,
                 Type = "image"
             });
 
